0a1f61edcdb7ac28a91a83074dc9ebe2d7301c10
Matias4kd <matiasfourcade6@gmail.com>
2024-05-23 19:38:31 -0300

    a

---
+++ Ferreteria/AppManager.h
@@ -0,0 +1,20 @@
+#ifndef APPMANAGER_H_INCLUDED
+#define APPMANAGER_H_INCLUDED
+
+class AppManager{
+
+    private:
+
+
+
+
+    public:
+
+
+
+
+
+
+};
+
+#endif // APPMANAGER_H_INCLUDED
--- Ferreteria/Producto.h
+++ Ferreteria/Producto.h
@@ -1,6 +1,20 @@
 #ifndef PRODUCTO_H_INCLUDED
 #define PRODUCTO_H_INCLUDED
 
+class Producto{
 
+    private:
+
+
+
+
+    public:
+
+
+
+
+
+
+};
 
 #endif // PRODUCTO_H_INCLUDED
